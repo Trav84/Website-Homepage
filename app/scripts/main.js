e79a804d7d07f076b51cf6e17c590c0c01fa32d2
Travis Czerw <travis.czerw@gmail.com>
2015-02-22 14:52:05 -0600

    Continued to work on homepage.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,2 +1,36 @@
 /* jshint devel:true */
+$(document).ready(function() {
+	'use strict';
 
+	var about = $('.about');
+	var skills = $('.skills');
+	var work = $('.work');
+	var contact = $('.contact');
+	var body = $('body');
+	var fundamentals = $('.fund');
+	var tools = $('#tools');
+	var libraries = $('#libs');
+	var frameworks = $('#frameworks');
+
+	$('#about').click(function() {
+		body.animate({scrollTop: about.offset().top-10});
+	});
+
+	$('#skills').click(function() {
+		body.animate({scrollTop: skills.offset().top-10});
+	});
+
+	$('#work').click(function() {
+		body.animate({scrollTop: work.offset().top-10});
+	});
+
+	$('#contact').click(function() {
+		body.animate({scrollTop: contact.offset().top});
+	});
+
+	$(fundamentals).click(function() {
+		fundamentals.toggleClass('clickedSet');
+		console.log('click');
+	});
+
+});
